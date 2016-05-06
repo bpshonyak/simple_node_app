6f2f1b0f86054dfd034ad56f1a73e7dbdc86e0f0
Bogdan Pshonyak <bpshonyak@live.com>
2016-05-06 13:12:24 -0700

    installed git-guppy pre commit

--- gulp.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by bogdan on 5/6/16.
- */
-var gulp = require('gulp');
-
-gulp.task('default', function() {
-  // place code for your default task here
-});
---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bogdan on 5/6/16.
+ */
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('pre-commit', ['unit']);
+
+gulp.task('unit', function() {
+  return gulp.src('./test/test-server.js', {read: false})
+      // gulp-mocha needs filepaths so you can't have any plugins before it
+      .pipe(mocha({reporter: 'nyan'}))
+});
