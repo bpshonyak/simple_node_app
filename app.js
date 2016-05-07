7f38b5b472e94cfb3b756a35ae0ddc0fe15f2f2b
Bogdan Pshonyak <bpshonyak@live.com>
2016-05-07 01:30:14 -0700

    serving files

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 var app = require("./server");
 
 
-app.listen(1337);
+app.listen(process.env.PORT || 8080, function () {
+  console.log("Listening on Port 8080");
+});
 
 // server.listen(process.env.PORT || 1337); //leet port
--- server.js
+++ server.js
@@ -2,26 +2,19 @@
  * Created by bogdan on 5/5/16.
  */
 var methods = require("./local_modules/methods");
-var http = require("http"); //http module
-
 
 methods.getUsers();
-
-var engine = function (req, res) {
-
-  res.writeHead("200", {'Content-Type': 'text/plain'});
-  res.end("Hello, World!");
-
-};
 
 var express = require("express");
 var app = module.exports = express();
 
+//serve files
+app.use("/assets", express.static(__dirname + "/assets"));
 
+//handle requests
 app.get("/", function (req, res) {
 
   res.send("Hello World! -express");
 
 });
 
-// var server = module.exports = http.createServer(engine);
