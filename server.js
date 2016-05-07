83c57f9a6487c6bb7b8da8242f9499e124635acd
Bogdan Pshonyak <bpshonyak@live.com>
2016-05-07 01:35:11 -0700

    serving html files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * Created by bogdan on 5/5/16.
  */
 var methods = require("./local_modules/methods");
+var path = require("path");
 
 methods.getUsers();
 
@@ -14,7 +15,7 @@ app.use("/assets", express.static(__dirname + "/assets"));
 //handle requests
 app.get("/", function (req, res) {
 
-  res.send("Hello World! -express");
+  res.sendFile("index.html", {root: path.join(__dirname, "./files")});
 
 });
 
